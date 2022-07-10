787541dd6e69330f1b56710b251a5c3d3a8a014a
Alexandre Junior <alexandrejr.mc@gmail.com>
2022-07-10 13:57:33 -0300

    feat: daily unit tests

---
+++ api-investments/src/daily/test/daily.resolver.spec.ts
@@ -0,0 +1,45 @@
+import { HttpService } from '@nestjs/axios';
+import { Test, TestingModule } from '@nestjs/testing';
+import { of } from 'rxjs';
+import { dailyMock, dailyMockDTO } from '../../mocks/dailys';
+import { DailyResolver } from '../daily.resolver';
+import { DailyService } from '../daily.service';
+
+describe('DailyResolver', () => {
+  let service: DailyService;
+  let resolver: DailyResolver;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        DailyService,
+        DailyResolver,
+        {
+          provide: HttpService,
+          useValue: {
+            get: jest.fn(() =>
+              of({
+                data: dailyMock,
+              }),
+            ),
+          },
+        },
+      ],
+    }).compile();
+    service = module.get<DailyService>(DailyService);
+    resolver = new DailyResolver(service);
+  });
+  it('should be defined', () => {
+    expect(resolver).toBeDefined();
+  });
+  describe('daily', () => {
+    it('should find all dailys', async () => {
+      const dailyfound = await resolver.daily({
+        function: 'TIME_SERIES_DAILY',
+        symbol: 'GOOG',
+        apikey: '9TVXPG5ISQ47OAVJ',
+      });
+      expect(dailyfound).toStrictEqual(dailyMockDTO);
+    });
+  });
+});
---
+++ api-investments/src/daily/test/daily.service.spec.ts
@@ -0,0 +1,40 @@
+import { HttpService } from '@nestjs/axios';
+import { Test, TestingModule } from '@nestjs/testing';
+import { of } from 'rxjs';
+import { dailyMock, dailyMockDTO } from '../../mocks/dailys';
+import { DailyService } from '../daily.service';
+
+describe('DailyService', () => {
+  let service: DailyService;
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        DailyService,
+        {
+          provide: HttpService,
+          useValue: {
+            get: jest.fn(() =>
+              of({
+                data: dailyMock,
+              }),
+            ),
+          },
+        },
+      ],
+    }).compile();
+    service = module.get<DailyService>(DailyService);
+  });
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+  });
+  describe('findAll', () => {
+    it('should find all dailys', async () => {
+      const dailyfound = await service.findAll({
+        function: 'TIME_SERIES_DAILY',
+        symbol: 'GOOG',
+        apikey: '9TVXPG5ISQ47OAVJ',
+      });
+      expect(dailyfound).toStrictEqual(dailyMockDTO);
+    });
+  });
+});
