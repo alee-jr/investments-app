3a1ea06bfb990ae46e6b0f85c25fa3884d36b055
Alexandre Junior <alexandrejr.mc@gmail.com>
2022-07-14 22:16:06 -0300

    fix: remove unused type

--- front-investments/src/types/investments.type.ts
+++
@@ -1,7 +0,0 @@
-import { Daily } from "./daily.type";
-import { NewsSentiment } from "./news-sentiment.type";
-
-export interface Investments {
-  daily: Daily[];
-  newsSentiment: NewsSentiment[];
-}
