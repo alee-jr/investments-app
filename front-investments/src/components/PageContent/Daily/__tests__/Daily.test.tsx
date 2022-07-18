2eda762a2572e4e11c52aea134c0a3f9792eca25
Alexandre Junior <alexandrejr.mc@gmail.com>
2022-07-18 17:11:30 -0300

    feat: adding daily component unit test

---
+++ front-investments/src/components/PageContent/Daily/__tests__/Daily.test.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import { MockedProvider } from "@apollo/client/testing";
+import _ from "lodash";
+import "jest-canvas-mock";
+import Daily from "..";
+import { GET_ALL_DAILY } from "../../../../graphql/queries";
+import { daily } from "../../../../mock/daily.mock";
+
+const mocks = [
+  {
+    request: {
+      query: GET_ALL_DAILY,
+    },
+    result: {
+      data: {
+        daily,
+      },
+    },
+  },
+];
+
+describe("graph component", () => {
+  it("should render daily", async () => {
+    const component = render(
+      <MockedProvider mocks={mocks} addTypename={false}>
+        <Daily />
+      </MockedProvider>
+    );
+    expect(component).toBeTruthy();
+  });
+});
